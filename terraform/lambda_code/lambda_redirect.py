af891c8374b2514566c3d82fa24aebc120b177f5
Han-Lon <56945456+Han-Lon@users.noreply.github.com>
2021-11-29 20:36:43 -0600

    Add logging to Lambda function

--- terraform/lambda_code/lambda_redirect.py
+++ terraform/lambda_code/lambda_redirect.py
@@ -4,14 +4,19 @@ from urllib import parse
 
 
 def lambda_handler(event, context):
+    print(f"Event submitted to the Lambda: {event}")
     # Get the site we want to redirect to from the "proxy" part of the submitted URL (from API Gateway). Use urllib to escape URL-unfriendly characters
     redirect_site = parse.quote(event["pathParameters"]["proxy"])
+
+    print(f"Site submitted by user to redirect to: {redirect_site}")
 
     # Build our response object as a Python dict object
     response = dict()
     response["statusCode"] = 302
     response["body"] = json.dumps(dict())
     response["headers"] = {"Location": f"https://www.{redirect_site}.com"}
+
+    print(f"Response being returned to API Gateway, then to the user: {response}")
 
     return response
 
