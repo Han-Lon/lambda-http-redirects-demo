298e31726fc3f138108a9da9fecd1b08247910dd
Han-Lon <56945456+Han-Lon@users.noreply.github.com>
2021-12-01 19:18:50 -0600

    Add docs/ folder with draw.io documentation. Add cloudwatch log group to redirect-lambda.tf so I can set a default log retention setting of 1 day

--- terraform/redirect-lambda.tf
+++ terraform/redirect-lambda.tf
@@ -57,20 +57,12 @@ resource "aws_lambda_permission" "redirect-lambda-permission" {
 }
 
 
+# The CloudWatch log group that the Lambda function will write logs to
+resource "aws_cloudwatch_log_group" "redirect-lambda-log-group" {
+  name              = "/aws/lambda/${aws_lambda_function.redirect-lambda-function.function_name}"
+  retention_in_days = 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  tags = {
+    Name = "redirect-example-lambda-log-group"
+  }
+}
